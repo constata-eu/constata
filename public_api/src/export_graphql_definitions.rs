85f8419266b105515044eaf00e3be7c5738a5f5e
nubis <yo@nubis.im>
2023-03-14 22:02:02 -0300

    disable export graphql definitions

--- public_api/src/export_graphql_definitions.rs
+++ public_api/src/export_graphql_definitions.rs
@@ -1,9 +1,12 @@
-mod controllers;
+/*mod controllers;
 pub use controllers::*;
 pub use constata_lib::models::Decimal;
+*/
 fn main() {
   println!("Disabled until all public api is a library");
+  /*
   let schema = controllers::certos::public_graphql::new_graphql_schema().as_schema_language();
   std::fs::write("public_api_schema.graphql", &schema).unwrap();
   std::fs::write("public_api_queries.graphql", graphql_queries_from_schema::generate_all(&schema).unwrap()).unwrap();
+  */
 }
