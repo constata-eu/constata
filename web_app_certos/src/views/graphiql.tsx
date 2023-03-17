5d8e6bdf73f9b8bc727c455b0f90bd745b640d73
LuchoLeonel <carrenoluciano@gmail.com>
2023-03-17 14:56:57 -0300

    wip

--- web_app_certos/src/views/graphiql.tsx
+++ web_app_certos/src/views/graphiql.tsx
@@ -1,9 +1,26 @@
 
+import { GraphiQL } from "graphiql";
+import { GraphiQLProvider, QueryEditor } from '@graphiql/react';
+import { createGraphiQLFetcher } from '@graphiql/toolkit';
+
+import 'graphiql/graphiql.css';
 
 const Graphiql = () => {
-    return(
-        <p>HOLA</p>
-    )
+  const fetcher = createGraphiQLFetcher({
+    url: 'https://localhost:8000/graphql',
+   });
+
+   
+  let myHeaders = {Authentication: "myheader"};
+
+  return(
+    <>
+     <div className="graphiql-container">
+      HOLAAA
+    </div>
+      <GraphiQL fetcher={fetcher} headers={myHeaders} />
+    </>
+  )
 }
 
 export default Graphiql;
