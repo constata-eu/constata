96dc20138242d1de0c3068606f24c2c206863cdf
bronxi47 <hbenja47@gmail.com>
2023-04-24 18:24:50 -0300

    wip

--- integration_tests/tests/cli.rs
+++ integration_tests/tests/cli.rs
@@ -32,33 +32,77 @@ mod cli {
     }
     */
 
-    api_integration_test!{ account_state(db, _chain)
-      let alice = db.alice().await;
-      alice.verify_email("alice@example.com").await;
-      alice.write_signature_json_artifact();
-
-      let mut json = run_command_json("account-state", &[]);
-      assert_eq!(json.get("id").unwrap(), 1);
-      /*
-      json = run_command_json("account-state", &[]);
-      assert_eq!(json.get("id").unwrap(), 1);
-      */
-    }
-
     api_integration_test!{ create_issuance_from_json(db, _chain)
       db.alice().await.write_signature_json_artifact();
 
-      let json = run_command_json("create-issuance-from-json", &[
+      let json1 = run_command_json("create-issuance-from-json", &[
         "name_of_the_issuance",
         "--new-logo-text=testing",
         "--new-kind=badge",
         "--new-name=my_template",
       ]);
 
+      let json2 = run_command_json("create-issuance-from-json", &[
+        "name_of_the_issuance_2",
+        "--new-logo-text=second",
+        "--new-kind=diploma",
+        "--new-name=my_template2",
+      ]);
+
+      let all_issuances = run_command_json("all-issuances", &[]);
+
+      let issuance_state = run_command("issuance-state", &["1", "received"]);
+
+      let all_issuances_id = run_command_json("all-issuances", &["--ids", "2"]);
+
+      println!("{}", &serde_json::to_string_pretty(&json1)?);
+
+      println!("{}", &serde_json::to_string_pretty(&all_issuances)?);
+
+      println!("{}", (&issuance_state));
+
+      //println!("{}", &serde_json::to_string_pretty(&issuance_state)?);
+      println!("{}", &serde_json::to_string_pretty(&all_issuances_id)?);
+
+
+      assert_eq!(json1.get("templateName").unwrap(), "my_template");
+      assert_eq!(json2.get("templateKind").unwrap(), "DIPLOMA");
+      assert_eq!((&all_issuances_id["allIssuances"][0]["templateId"]), 2);
+      assert_eq!(issuance_state, "true\n");
+    }
+
+    api_integration_test!{ account_state(db, _chain)
+      let alice = db.alice().await;
+      alice.verify_email("alice@example.com").await;
+      alice.write_signature_json_artifact();
+
+      let json = run_command_json("account-state", &[]);
+      assert_eq!(json.get("id").unwrap(), 1);
+
+      println!("{}", &serde_json::to_string_pretty(&json)?);
+      /*
+      json = run_command_json("account-state", &[]);
+      assert_eq!(json.get("id").unwrap(), 1);
+      */
+    }
+
+    /*
+    api_integration_test!{ all_issuances(db, _chain)
+      let _alice = db.alice().await;
+
+      let json = run_command_json("all-issuances", &[]);
+      //assert_eq!(json.get("id").unwrap(), 1);
+
       println!("{}", &serde_json::to_string_pretty(&json)?);
 
-      assert_eq!(json.get("templateName").unwrap(), "my_template");
-      assert_eq!(json.get("templateKind").unwrap(), "BADGE");
+    }
+    */
+    api_integration_test!{ help(db, _chain)
+      db.alice().await.write_signature_json_artifact();
+
+      let no_json = run_command("--help", &[]);
+
+      println!("{}", (&no_json));
     }
 
     fn run_command(command: &str, args: &[&str]) -> String {
