3015cf7fc040fbac1abcf979b63e37560976d205
bronxi47 <hbenja47@gmail.com>
2023-04-28 11:44:24 -0300

    Completed issuances tests

--- integration_tests/tests/cli.rs
+++ integration_tests/tests/cli.rs
@@ -2,6 +2,9 @@ mod cli {
   constata_lib::describe_one! {
     use integration_tests::*;
     use assert_cmd::Command;
+    use serde_json::Value::Null;
+
+
     /*
      *  This test is commented until we have a new release.
     test!{ cli_standard_workflow
@@ -32,7 +35,7 @@ mod cli {
     }
     */
 
-    api_integration_test!{ create_issuance_from_json(db, mut _chain)
+    api_integration_test!{ create_issuances(db, mut _chain)
       db.alice().await.write_signature_json_artifact();
 
       let issuance_1 = run_command_json("create-issuance-from-json", &[
@@ -49,67 +52,44 @@ mod cli {
         "--new-name=2nd_template",
       ]);
 
-      let create_issuance_from_csv  = run_command_json("create-issuance-from-csv", &["--csv-file", "integration_tests/static/cli_test_issuances_from_csv.csv", "first_csv_test", "-t", "2"]);
-
-      let all_issuances = run_command_json("all-issuances", &[]);
-
+      let create_issuance_from_csv  = run_command_json("create-issuance-from-csv", &["--csv-file", "integration_tests/static/cli_test_issuances_from_csv.csv", "first_csv_test_simpson", "-t", "2"]);
       let issuance_state = run_command("issuance-state", &["1", "received"]);
-
-      let all_issuances_id_2 = run_command_json("all-issuances", &["--ids", "2"]);
+      let all_issuances_id_eq_2 = run_command_json("all-issuances", &["--id-eq", "2"]);
             
       db.site.request().create_all_received().await?; // Ahora se crean todos los documentos.
 
-      let all_issuances_id_3 = run_command_json("all-issuances", &["--ids", "3"]);
+      let all_issuances_id_eq_3 = run_command_json("all-issuances", &["--id-eq", "3"]);
 
       run_command("preview-entry", &["9", "target/artifacts/cli_preview.html"]);
-
       run_command("sign-issuance", &["2"]);
 
       run_command("sign-issuance", &["3"]);
 
-      let all_issuances_id_2_founded = run_command_json("all-issuances", &["--ids", "2"]);
+      let all_issuances_id_eq_2_founded = run_command_json("all-issuances", &["--id-eq", "2"]);
+      let all_issuances_id_eq_empty = run_command_json("all-issuances", &["--id-eq", "21"]);
 
       _chain.fund_signer_wallet();
       _chain.simulate_stamping().await;
-  
       db.site.request().try_complete().await?;
       
       run_command("issuance-export", &["3", "target/artifacts/cli_issuance_export.csv"]);
 
-      let all_issuances_id_2_completed = run_command_json("all-issuances", &["--ids", "2"]);
+      let all_issuances_id_eq_2_completed = run_command_json("all-issuances", &["--id-eq", "2"]);
+      let all_issuances_name_like = run_command_json("all-issuances", &["--name-like", "simpson"]);
 
-      let all_issuances_after_sign_and_stamping = run_command_json("all-issuances", &[]);
-
-      
-
-      //let all_templates = run_command_json("all-templates", &[]);
-      
-      println!("{}", &serde_json::to_string_pretty(&issuance_1)?);
-
-      println!("{}", &serde_json::to_string_pretty(&all_issuances)?);
-
-      println!("{}", (&issuance_state));
-
-      //println!("{}", &serde_json::to_string_pretty(&issuance_state)?);
-      println!("{}", &serde_json::to_string_pretty(&all_issuances_id_2)?);
-      println!("{}", &serde_json::to_string_pretty(&all_issuances_id_2_founded)?);
-      println!("{}", &serde_json::to_string_pretty(&all_issuances_id_2_completed)?);
-      println!("{}", &serde_json::to_string_pretty(&create_issuance_from_csv)?);
-      println!("{}", &serde_json::to_string_pretty(&all_issuances_after_sign_and_stamping)?);
-      //println!("{}", &serde_json::to_string_pretty(&all_templates)?);
-      //println!("this is the CHAIN: {:?}", (&chain_output));
-
-
+      run_command("entry-html-export", &["4", "target/artifacts/cli_entry_export.html"]);
+            
       assert_eq!((&issuance_1["templateName"]), "my_template");
       assert_eq!((issuance_2["templateKind"]), "DIPLOMA");
-      assert_eq!((&all_issuances_id_2["allIssuances"][0]["templateId"]), 2);
-      assert_eq!((&all_issuances_id_2["allIssuances"][0]["state"]), "received");
-      assert_eq!((&all_issuances_id_3["allIssuances"][0]["state"]), "created");
-      assert_eq!((&all_issuances_id_2_founded["allIssuances"][0]["state"]), "signed");
-      assert_eq!((&all_issuances_id_2_completed["allIssuances"][0]["state"]), "completed");
+      assert_eq!((&all_issuances_id_eq_2["allIssuances"][0]["templateId"]), 2);
+      assert_eq!((&all_issuances_id_eq_2["allIssuances"][0]["state"]), "received");
+      assert_eq!((&all_issuances_id_eq_3["allIssuances"][0]["state"]), "created");
+      assert_eq!((&all_issuances_id_eq_2_founded["allIssuances"][0]["state"]), "signed");
+      assert_eq!((&all_issuances_id_eq_2_completed["allIssuances"][0]["state"]), "completed");
       assert_eq!((create_issuance_from_csv["entriesCount"]), 10);
       assert_eq!(issuance_state, "true\n");
-      
+      assert_eq!((all_issuances_id_eq_empty["allIssuances"][0]), Null);
+      assert_eq!((all_issuances_name_like["allIssuances"][0]["name"]), "first_csv_test_simpson");
     }
 
     api_integration_test!{ account_state(db, _chain)
@@ -120,7 +100,6 @@ mod cli {
       let json = run_command_json("account-state", &[]);
       assert_eq!(json.get("id").unwrap(), 1);
 
-      println!("{}", &serde_json::to_string_pretty(&json)?);
       /*
       json = run_command_json("account-state", &[]);
       assert_eq!(json.get("id").unwrap(), 1);
