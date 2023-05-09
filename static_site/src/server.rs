a3c00c67132b40062f59e9d6ab6482070f80bd47
nubis <yo@nubis.im>
2023-05-09 03:41:34 -0300

    Added CORS allow all to static site for nostr.json

--- static_site/src/server.rs
+++ static_site/src/server.rs
@@ -5,6 +5,7 @@ pub use i18n::{Renderer, Lang};
 pub use rocket::*;
 pub use error::LocalizedResult;
 pub use config::Config;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 #[get("/")]
 fn index(lang: Lang, config: &State<Config>) -> LocalizedResult {
@@ -22,7 +23,17 @@ fn public(lang: Lang, path: PathBuf, config: &State<Config>) -> LocalizedResult 
 }
 
 pub async fn rocket() -> Rocket<Build> {
+  let cors = rocket_cors::CorsOptions {
+    allowed_origins: AllowedOrigins::all(),
+    allowed_methods: vec![http::Method::Get].into_iter().map(From::from).collect(),
+    allowed_headers: AllowedHeaders::all(),
+    allow_credentials: true,
+    ..Default::default()
+  }
+  .to_cors().expect("No pude crear el CORS.");
+
   build()
+    .attach(cors)
     .attach(rocket::fairing::AdHoc::config::<config::Config>())
     .mount("/", routes![ index, public, well_known ])
 }
