79425e7baa650fb092b9561fdaf38a3484df985b
nubis <yo@nubis.im>
2023-05-09 05:09:10 -0300

    Replaced rocket cors with actual wildcard allow origin

--- static_site/src/server.rs
+++ static_site/src/server.rs
@@ -5,7 +5,11 @@ pub use i18n::{Renderer, Lang};
 pub use rocket::*;
 pub use error::LocalizedResult;
 pub use config::Config;
-use rocket_cors::{AllowedHeaders, AllowedOrigins};
+use rocket::{
+  http::Header,
+  fairing::{Fairing, Info, Kind}
+};
+
 
 #[get("/")]
 fn index(lang: Lang, config: &State<Config>) -> LocalizedResult {
@@ -22,18 +26,28 @@ fn public(lang: Lang, path: PathBuf, config: &State<Config>) -> LocalizedResult 
   super::render(lang, path, config)
 }
 
-pub async fn rocket() -> Rocket<Build> {
-  let cors = rocket_cors::CorsOptions {
-    allowed_origins: AllowedOrigins::all(),
-    allowed_methods: vec![http::Method::Get].into_iter().map(From::from).collect(),
-    allowed_headers: AllowedHeaders::all(),
-    allow_credentials: true,
-    ..Default::default()
-  }
-  .to_cors().expect("No pude crear el CORS.");
+pub struct CORS;
 
+#[rocket::async_trait]
+impl Fairing for CORS {
+  fn info(&self) -> Info {
+    Info {
+      name: "Add CORS headers to responses",
+      kind: Kind::Response
+    }
+  }
+
+  async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+    response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+    response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+    response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+    response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+  }
+}
+
+pub async fn rocket() -> Rocket<Build> {
   build()
-    .attach(cors)
+    .attach(CORS)
     .attach(rocket::fairing::AdHoc::config::<config::Config>())
     .mount("/", routes![ index, public, well_known ])
 }
